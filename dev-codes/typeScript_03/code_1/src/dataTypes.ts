6955cea9c7523d2f51b63eb0561b4fe2555df4f7
jbjzeehad <ilamta.97@gmail.com>
2025-12-24 21:45:39 +0600

    coding practise for typescript data type, setup and oyher confiquration

---
+++ dev-codes/typeScript_03/code_1/src/dataTypes.ts
@@ -0,0 +1,27 @@
+// string
+
+let lastName: string = "Hanma";
+
+// number
+
+let employeeId: number = 25056;
+
+// boolean
+
+let isRequired: boolean = true;
+
+// undefined
+
+let age: undefined = undefined;
+
+// null
+
+let address: null = null;
+
+// array
+
+let fruitsName: string[] = ["apple", "mango", "banana"];
+
+// tuple
+
+let nameRollAttend: [string, number, boolean] = ["Mashle", 90, false];
---
+++ dev-codes/typeScript_03/code_1/src/index.ts
@@ -0,0 +1,3 @@
+const firstName: string = "Jubayer";
+
+console.log(firstName);
