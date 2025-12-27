1fce8abafd011999369161d54b28e58f5daf16b1
jbjzeehad <ilamta.97@gmail.com>
2025-12-27 23:33:23 +0600

    learned how destructuring work in typescript

--- dev-codes/typeScript_03/code_1/src/destructuring.ts
+++ dev-codes/typeScript_03/code_1/src/destructuring.ts
@@ -1,1 +1,23 @@
-// destructuring
+//object destructuring
+
+const user = {
+  id: 345,
+  name: {
+    firstName: "Jubayer",
+    middleName: "Bin",
+    lastName: "Jaman",
+  },
+  contactNo: "01798329264",
+  address: "Uganda",
+};
+
+const {
+  contactNo,
+  name: { middleName },
+} = user;
+
+//array destructuring
+
+const myFriends = ["Rahim", "Karim", "Lorim", "Borim", "Norim", "Shorim"];
+
+const [, , bestFriend, ...rest] = myFriends;
